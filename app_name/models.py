4dcaf778b1524736fe2caa5f6283a0f3d4575cfc
godsgived <bobagaga20081127@gmail.com>
2026-06-20 08:02:20 +0300

    fix some things

--- app_name/models.py
+++ app_name/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.exceptions import ValidationError
 
+
 class Product(models.Model):
     ...
 
@@ -9,7 +10,10 @@ class Product(models.Model):
 
     def clean(self):
         super().clean()
+        email_exists = (
+            Product.objects.filter(email=self.email).exclude(pk=self.pk).exists()
+        )
 
     def save(self, *args, **kwargs):
-        self.full_clean() 
+        self.full_clean()
         super().save(*args, **kwargs)
--- exam/urls.py
+++ exam/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.split),
-    path('app_name/', include('app_name.urls')), 
+    path('admin/', admin.site.urls),
+    path('', include('app_name.urls')), 
 ]
